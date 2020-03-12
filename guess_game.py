5f5c1cdeb32cc691d0d27f0b88e41a1f91bfa5d2
Amaka <amakaumeh01@gmail.com>
2020-03-12 23:43:37 +0100

    Initial commit

---
+++ guess_game.py
@@ -0,0 +1,12 @@
+secret_number = 10
+guess_limit = 3
+guess_count = 0
+
+while guess_count < guess_limit:
+    guess_count += 1
+    guess = int(input("Guess my secret number: "))
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+    print("Sorry, You failed!")
